e039e16de6a433082cf8f2b65458a26c63fdc4b4
SN <snbibhudutta216@gmail.com>
2023-12-27 12:15:53 +0530

    boilerplate added

---
+++ boilerplate.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define fo(type,i,k,n) for(type i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define MOD 1000000007
+using namespace std;
+void solve()
+{
+    //CODE
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    #endif 
+    int tt ; 
+    cin>>tt ; 
+    while(tt--)
+    {
+        solve() ;
+    }
+}
